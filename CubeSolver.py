984affeeb518d950f333f247673d09b20acf591d
greggyd564 <greggyd564@gmail.com>
2025-12-01 10:37:48 -0800

    RubikAStar format is now 3x3 per face

--- CubeSolver.py
+++ CubeSolver.py
@@ -5,7 +5,16 @@ from Cube import Cube
 class CubeSolver:
 
     def RubikAStar(self, input_obj):
-        cube = Facelet_to_Cube.facelet_to_cube(input_obj)
+        formatted_obj = {}
+        for i in ("up", "down", "left", "right", "front", "back"):
+            face = input_obj[i]
+            flat = []
+            for j in range(3):
+                flat.append(face[j][0])
+                flat.append(face[j][1])
+                flat.append(face[j][2])
+            formatted_obj[i] = flat
+        cube = Facelet_to_Cube.facelet_to_cube(formatted_obj)
         solver = IDASolver(cube)
         return solver.solve(max_depth=21)
 
